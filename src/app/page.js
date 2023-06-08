d3f2881c5f2e8e90f650d860724e7d26a0e2b184
Long Ha Hoang <long@shenlong.de>
2023-06-08 15:06:48 +0200

    ADD: temp style

--- src/app/page.js
+++ src/app/page.js
@@ -5,19 +5,27 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import Carousel from "./components/Carousel";
+import ZPattern from "./components/ZPattern";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main>
+      <StyledMain>
         <HeroSection />
-        <p>Under Construction</p>
         <Carousel />
-      </main>
+        <ZPattern />
+      </StyledMain>
       <Footer />
     </>
   );
 }
 
-const StyledMain = styled.main``;
+const StyledMain = styled.main`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
