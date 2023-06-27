9ba8aea41e53f4a993cedd9c079714639cd1150e
Long Ha Hoang <github@ha.mozmail.com>
2023-06-27 12:56:40 +0200

    ADD: google maps link

--- src/app/components/Location.js
+++ src/app/components/Location.js
@@ -22,7 +22,13 @@ export default function Location() {
         </p>
         <Link href={"tel://+491234567890"}>+49 123 4567890</Link>
         <Link href={"mailto://email@example.com"}>email@example.com</Link>
-        <button>Tattoo Studio</button>
+        <TattooStudioLink
+          href={
+            "https://www.google.de/maps/place/Drawink+Nerds/@52.5236988,13.449067,18.58z/data=!3m1!5s0x47a84e6eed6bf0e7:0xb72dd87f0219e7f2!4m15!1m8!3m7!1s0x47a84e6eed39749f:0x7ed6c4f2eced5974!2sPetersburger+Str.+28,+10249+Berlin!3b1!8m2!3d52.5236708!4d13.4489381!16s%2Fg%2F11c4khc9sl!3m5!1s0x47a84e6eed90ed51:0x91cd300c7c30d380!8m2!3d52.5236418!4d13.4488917!16s%2Fg%2F11g69hf4tg?entry=ttu"
+          }
+        >
+          Tatto Studio
+        </TattooStudioLink>
       </AdressContainer>
     </LocationSection>
   );
@@ -72,4 +78,12 @@ const AdressContainer = styled.div`
   align-items: flex-start;
   justify-content: center;
   gap: 0.5rem;
+`;
+
+const TattooStudioLink = styled(Link)`
+  padding: 0.5rem;
+  border: solid white;
+  border-radius: 0.25rem;
+  justify-self: center;
+  align-self: center;
 `;
