4387215767e9240290f8f459682c8d8267f49bbd
Long Ha Hoang <github@ha.mozmail.com>
2023-07-03 19:30:01 +0200

    ADD: key to burgerMenuLinks list

--- src/app/components/BurgerMenuLinks.js
+++ src/app/components/BurgerMenuLinks.js
@@ -1,19 +1,22 @@
+"use client";
+
 import { styled } from "styled-components";
 import Link from "next/link";
+import { v4 as uuidv4 } from "uuid";
 
 export default function BurgerMenuLinks() {
   return (
     <BurgerMenuLinkList>
-      <li>
+      <li key={uuidv4()}>
         <Link href={"/"}>Home</Link>
       </li>
-      <li>
+      <li key={uuidv4()}>
         <Link href={"/tattoogallery"}>Tattoo Gallery</Link>
       </li>
-      <li>
+      <li key={uuidv4()}>
         <Link href={"/artgallery"}>Art Gallery</Link>
       </li>
-      <li>
+      <li key={uuidv4()}>
         <Link href={"/about"}>About Me</Link>
       </li>
     </BurgerMenuLinkList>
