df4fcc4396ab3411e86d64d115434c03543be2b7
Long Ha Hoang <github@ha.mozmail.com>
2023-09-18 14:51:01 +0200

    add css styles to contact button

--- src/app/components/ContactForm.js
+++ src/app/components/ContactForm.js
@@ -1,7 +1,23 @@
+import styled from "styled-components";
+
 export default function ContactForm() {
   return (
-    <section>
-      <button>Kontaktformular</button>
-    </section>
+    <StyledSection>
+      <StyledButton>Kontaktformular</StyledButton>
+    </StyledSection>
   );
 }
+
+const StyledSection = styled.section`
+  width: 100%;
+  height: 3rem;
+  border: solid green;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledButton = styled.button`
+  width: 30%;
+  height: 100%;
+`;
